0dc2badcbdb425d221253552a6f9593e338b135c
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-29 22:54:57 -0400

    Function to get weather from open meteo api

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function getWeather() {
-  const apiKey = 'YOUR-API-KEY';
   const city = document.getElementById('city').value;
 
   if (!city) {
@@ -7,8 +6,13 @@ function getWeather() {
     return;
   }
 
-  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apidKey}`
-  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecase?q=${city}&appid=${apidKey}`;
+  const latitude = 0;
+  const longitude = 0;
+
+  const currentWeatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode`;
+
+  const forecastUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode`;
+
   
   fetch (currentWeatherUrl)
     .then(response => response.json())
@@ -29,4 +33,11 @@ function getWeather() {
       console.error('Error fetching hourly forecast data:', error);
       alert('Error fetching hourly forecast data. Please try again.');
     });
+}
+
+function displayWeather(data) {
+  const tempDivInfo = document.getElementById('temp-div');
+  const weatherInfoDiv = document.getElementById('weather-info');
+  const weatherIcon = document.getElementById('weather-icon');
+  const hourlyForecastDiv = document.getElementById('hourly-forecast');
 }
