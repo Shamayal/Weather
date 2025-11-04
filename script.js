2a0704c6445c7410dffdc8942c9549371b730b88
Shamayal Syed <shamayalsyed9@gmail.com>
2025-11-03 22:21:22 -0500

    Added variables for html elements

---
+++ script.js
@@ -0,0 +1,8 @@
+let apiKey;
+let apiURl;
+
+const cityInput = document.getElementById('city');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
