8905991bb684be7ce97ff3efa133ee645db7a0cc
Shamayal Syed <shamayalsyed9@gmail.com>
2025-11-05 20:03:51 -0500

    Added event listener for search button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let apiKey = API_KEY;
-let apiURl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiURl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const cityInput = document.getElementById('city');
 const searchButton = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+  const city = cityInput.value;
+  if (city) {
+    getWeather(city);
+  }
+})
