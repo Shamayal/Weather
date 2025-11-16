5410d95e2cb3901304923e38ea0afd7ecd058828
Shamayal Syed <shamayalsyed9@gmail.com>
2025-11-15 20:34:41 -0500

    Return json data for city entered

--- public/script.js
+++ public/script.js
@@ -1,29 +1,45 @@
-let apiKey = API_KEY;
-const apiURl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = API_KEY;
 
-const cityInput = document.getElementById('city');
-const searchButton = document.getElementById('searchButton');
+const cityInput = document.querySelector('.city-input');
+const searchButton = document.querySelector('.search-btn');
 const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 
 searchButton.addEventListener('click', () => {
-  const city = cityInput.value;
-  if (city) {
-    getWeather(city);
+  if (cityInput.value.trim() != '') {
+    getWeather(cityInput.value);
+    cityInput.value = '';
+    cityInput.blur();
   }
 })
 
-function getWeather(city) {
-  fetch(`/weather?city=${city}`)
-    .then(response => response.json())
-    .then(data => {
-      locationElement.textContent = data.name;
-      temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-      descriptionElement.textContent = data.weather[0].description;
-      humidityElement.textContent = `${data.main.humidity}%`;
-    })
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-    });
+cityInput.addEventListener('keydown', (event) => {
+  if (event.key == 'Enter' && cityInput.value.trim() != '') {
+    getWeather(cityInput.value);
+    cityInput.value = '';
+    cityInput.blur();
+  }
+})
+
+async function fetchData(endPoint, city) {
+  const apiURl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
+  const response = await fetch(apiURl);
+  return response.json();
+}
+
+async function getWeather(city) {
+  const weatherData = await fetchData('weather', city);
+  console.log(weatherData);
+  // fetch(`/weather?city=${city}`)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     locationElement.textContent = data.name;
+  //     temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+  //     descriptionElement.textContent = data.weather[0].description;
+  //     humidityElement.textContent = `${data.main.humidity}%`;
+  //   })
+  //   .catch(error => {
+  //     console.error('Error fetching weather data:', error);
+  //   });
 }
