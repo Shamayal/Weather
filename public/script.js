a407469067cb0556479a7222606670b9482924d1
Shamayal Syed <shamayalsyed9@gmail.com>
2025-11-19 20:25:50 -0500

    formatting

--- public/script.js
+++ public/script.js
@@ -40,7 +40,6 @@ async function fetchData(endPoint, city) {
 }
 
 function getWeatherIcon(id) {
-  console.log(id)
   if (id <= 232) return 'thunderstorm.svg';
   if (id <= 321) return 'drizzle.svg';
   if (id <= 531) return 'rain.svg';
@@ -67,7 +66,6 @@ async function updateWeatherInfo(city) {
     showDisplaySection(notFoundSection);
     return;
   }
-  console.log(weatherData)
 
   const {
     name: location,
@@ -98,16 +96,12 @@ async function updateForecastsInfo(city) {
   forecastItemsContainer.innerHTML = ''
   forecastsData.list.forEach(forecastWeather => {
     if (forecastWeather.dt_txt.includes(timeTaken) && !forecastWeather.dt_txt.includes(todayDate)) {
-      console.log(forecastWeather);
       updateForecastItems(forecastWeather);
     }
   })
-  console.log(todayDate);
-  console.log(forecastsData);
 }
 
 function updateForecastItems(weatherData) {
-  console.log('weather data ' + weatherData);
   const {
     dt_txt: date,
     weather: [{ id}],
